888be6b5c8948200bddff2ab3f19d796676a1f26
Olivier Penhoat <openhoat@gmail.com>
2013-11-16 17:49:22 +0100

    enhancements

---
+++ spec/generateRandomStringSpec.js
@@ -0,0 +1,24 @@
+var util = require('../lib/hw-util');
+
+function checkGeneratedRandomString(length, filter, checkRegexp, loop) {
+  var i, result;
+  loop = loop || 1;
+  for (i = 0; i < loop; i++) {
+    result = util.generateRandomString(length, filter);
+    expect(result).toBeTruthy();
+    expect(result.length).toBe(length);
+    expect(result.match(new RegExp(util.format('%s{%s}', checkRegexp, length), 'g'))).toBeTruthy();
+  }
+}
+
+describe('generate random string', function () {
+  it('should generate a 5 length random string containing [a-z0-9]', function () {
+    checkGeneratedRandomString(5, null, '[a-z0-9]');
+  });
+  it('should generate a 3 length random string containing 0-9', function () {
+    checkGeneratedRandomString(3, '0123456789', '[0-9]');
+  });
+  it('should generate 20 times a 100 length random string containing X or y', function () {
+    checkGeneratedRandomString(100, 'Xy', '[Xy]', 20);
+  });
+});

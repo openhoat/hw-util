90ab0478844fa7f054a5361ecb73547b419f5db9
Olivier Penhoat <openhoat@gmail.com>
2013-11-24 19:13:17 +0100

    copy properties enhancements

---
+++ spec/copyPropertiesSpec.js
@@ -0,0 +1,26 @@
+var util = require('../lib/hw-util');
+
+describe('copy properties', function () {
+  it('should return a new object with a copy of some properties', function () {
+    var src, dest;
+    src = {
+      '__v': 0,
+      '_id': '52879cc8ceee8cad15000001',
+      'path1': 'so',
+      'path2': 'me',
+      'url': 'http://www.some.org',
+      'hits': 2,
+      'createdAt': '2013-11-16T16:26:48.505Z'
+    };
+    dest = util.copyProperties(src, null, false, ['__v'], { '_id': 'id' });
+    expect(dest).toBeTruthy();
+    expect(JSON.stringify(dest)).toBe(JSON.stringify({
+      'id': '52879cc8ceee8cad15000001',
+      'path1': 'so',
+      'path2': 'me',
+      'url': 'http://www.some.org',
+      'hits': 2,
+      'createdAt': '2013-11-16T16:26:48.505Z'
+    }));
+  });
+});
